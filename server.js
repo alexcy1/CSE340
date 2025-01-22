7cc274a4e4109d5b6abb0e7ad721ce1b2ec77905
alexcy1 <cyr23001@byui.edu>
2025-01-22 18:42:33 +0100

    Completed Week 03 Activity

--- server.js
+++ server.js
@@ -5,53 +5,86 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts");
-require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+require('express-ejs-layouts');
+
+const baseController = require('./controllers/baseController');
+const inventoryRoute = require('./routes/inventoryRoute');
+const utilities = require('./utilities/index');
+
+const env = require('dotenv').config();
+const app = express();
+const static = require('./routes/static');
 
 /* ******************************************
  * I'm trying To Set Dynamic Active Link Here
  ********************************************/
 app.use((req, res, next) => {
-  res.locals.currentRoute = req.path;
-  next();
+    res.locals.currentRoute = req.path;
+    next();
 });
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', './layouts/layout'); // not at views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 
 // Index Route
-app.get("/", (req, res) => {
-  res.render("index", {title: "Home"});
-})
+// app.get('/', (req, res) => {
+//     res.render('index', {title: 'Home'});
+// });
+app.get("/", utilities.handleErrors(baseController.buildHome));
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
 
 // Custom Route For Active Link
-app.get("/custom", (req, res) => {
-  res.render("custom", { title: "Custom" });
-});
+// app.get('/custom', (req, res) => {
+//     res.render('custom', {title: 'Custom'});
+// });
+
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+    next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+  })
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+    let nav = await utilities.getNav()
+    console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+    if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+    res.render("errors/error", {
+      title: err.status || 'Server Error',
+      message,
+      nav
+    })
+  })
+
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+    console.log(`app listening on ${host}:${port}`);
+});
