f34c45a16302f175ee1d868bed86eda6d4f04091
alexcy1 <cyr23001@byui.edu>
2025-01-28 19:18:54 +0100

    Corrected HTML

--- utilities/index.js
+++ utilities/index.js
@@ -205,8 +205,6 @@ Util.buildVehicleDetail = async (vehicle) => {
 };
 
 /* ****************************************
- * Middleware For Handling Errors
- * Wrap other function in this for
  * General Error Handling
  **************************************** */
 Util.handleErrors = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
